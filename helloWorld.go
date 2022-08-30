73fdfc24e3d7505dd366c6eba13dd730d7cf97dd
RemiArata <50224993+RemiArata@users.noreply.github.com>
2022-08-30 14:13:34 -0600

    Add files via upload

---
+++ helloWorld.go
@@ -0,0 +1,11 @@
+/*
+This is a simple Hello world function in go. This shows the basics of how to
+use the main package, import states, and the general syntax of a function.
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, world!")
+}
